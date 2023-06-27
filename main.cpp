6f6795e1abd1344abaabff72f39ea8eec193d9ea
Mihir-Paija <mihir21102003@gmail.com>
2023-06-27 18:44:59 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int opt;
+    cout << endl;
+    cout << "*****Welcome to Bank Management System*****" << endl << endl;
+    cout << "Login As" << endl;
+    cout << "1 : Customer " << endl;
+    cout << "2 : Staff" << endl;
+    cout << "3 : admin" << endl;
+    cout << "Or press \'-1\' for exit" << endl;
+    do
+    {
+        cout << "Select your option : ";
+        cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            /* code */
+            break;
+
+        case 2:
+            /* code */
+            break;
+
+        case 3:
+            /* code */
+            break;
+
+        case -1:
+            /* code */
+            break;    
+
+        default:
+            cout << "Invalid Option" << endl;
+            break;
+        }
+    } while (opt != -1);
+
+    cout << "***** Thanks for Visit *****" << endl;       
+
+    return 0;
+}
